c588f702a42a1027402c4804813703961217bf00
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-05-03 13:44:30 -0700

    card component with props

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,24 +1,22 @@
 import '../App.css'
 
-import swimmer from '/src/assets/card images/image 12.png'
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 
-export const Card = () => {
+export const Card = (props) => {
     return(
         <div className="card-container">
-            <img src={swimmer} 
+            <img src={props.img} 
                  className="card-image"
                  alt="Image of Katie Zaferes"/>
             <div className="card-stats">
                 <FontAwesomeIcon icon={faStar} style={{color: "#fe395c",}} />
-                <span>5.0</span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">{props.country}</span>
             </div>
-            <h2>Life lessons with Katie Zaferes</h2>
-            <p><span className="bold">From $136</span> / person</p>
+            <h2>{props.title}</h2>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     );
 }
