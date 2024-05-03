cfc6e67f326e56b5d6a655b456a4553ee0eb226d
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-05-03 08:02:34 -0700

    initial setup and addition of navbar

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import './NavBar.css'
+
+import logo from '/src/assets/airbnb 1.png'
+
+export const NavBar = () => {
+    return(
+        <div className="nav">
+            <img src={logo}/>
+        </div>
+    );
+}
